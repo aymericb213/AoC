be08a62bdf806fb6bbbcaf136421bf5fb3870459
aymericb213 <aymeric.beauchamp@protonmail.com>
2020-12-03 10:10:09 +0100

    Day 3

---
+++ 2020/03/part1.jl
@@ -0,0 +1,21 @@
+#https://adventofcode.com/2020/day/3
+matrix = []
+
+open("input.txt") do file
+    for line in eachline(file)
+        push!(matrix, split(strip(line), ""))
+    end
+end
+
+solution = 0
+sled = (1,1)
+
+while sled[2] <= size(matrix, 1)
+    if matrix[sled[2]][sled[1]] == "#"
+        global solution += 1
+    end
+    x = sled[1]+3 > size(matrix[1], 1) ? (sled[1]+3)%size(matrix[1],1) : sled[1]+3
+    global sled = (x, sled[2]+1)
+end
+
+println(solution)
---
+++ 2020/03/part2.jl
@@ -0,0 +1,30 @@
+#https://adventofcode.com/2020/day/3
+matrix = []
+
+open("input.txt") do file
+    for line in eachline(file)
+        push!(matrix, split(strip(line), ""))
+    end
+end
+
+slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+
+function slide(slope)
+    sled = (1,1)
+    trees = 0
+    while sled[2] <= size(matrix, 1)
+        if matrix[sled[2]][sled[1]] == "#"
+            trees += 1
+        end
+        x = sled[1]+slope[1] > size(matrix[1], 1) ? (sled[1]+slope[1])%size(matrix[1],1) : sled[1]+slope[1]
+        sled = (x, sled[2]+slope[2])
+    end
+    return trees
+end
+
+solution = []
+for slope in slopes
+    push!(solution, slide(slope))
+end
+println(solution)
+println(prod(solution))
